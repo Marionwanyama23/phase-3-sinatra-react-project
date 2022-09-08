081879b3496d56c86151c246a3b7308363dc5e4c
+ <+>
2022-09-08 17:35:23 +0300

    commit

---
+++ app/models/song.rb
@@ -0,0 +1,16 @@
+class Song < ActiveRecord::Base
+    belongs_to :band
+    
+    def self.render_all
+        self.all.map { |s| {
+          name: s.name,
+          band_id: s.band_id
+        }}
+    end
+
+    def self.find_by_path(path)
+        id = path.split("/songs/")[1].to_i
+        Song.find_by_id(id)
+    end
+
+end
