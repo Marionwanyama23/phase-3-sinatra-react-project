da3d04a02edad0379b868c409ce33b5960f5394d
+ <+>
2022-09-12 20:43:08 +0300

    commit

--- config/environment.rb
+++
@@ -1,6 +0,0 @@
-require "bundler/setup"
-Bundler.require
-
-require "json"
-require "rack/cors"
-require_all "app"
--- db/migrate/20210810141716_create_song.rb
+++
@@ -1,9 +0,0 @@
-class CreateSong < ActiveRecord::Migration[6.1]
-  def change
-    create_table :songs do |t| 
-      t.string :name
-      t.belongs_to :band
-      t.timestamps
-    end
-  end
-end
